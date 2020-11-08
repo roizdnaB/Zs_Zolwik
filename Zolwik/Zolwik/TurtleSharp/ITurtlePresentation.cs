e9e309a9c95da734619089125864f46b2e441475
Quackeee <krzysztofklak7@gmail.com>
2020-11-08 22:57:02 +0100

    migrate to core, add basic setup

---
+++ Zolwik/Zolwik/TurtleSharp/ITurtlePresentation.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TurtleSharp
+{
+    public interface ITurtlePresentation
+    {
+    }
+}
---
+++ Zolwik/Zolwik/TurtleSharp/Turtle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurtleSharp
+{
+    class Turtle
+    {
+        private ITurtlePresentation Presentation;
+    }
+}
