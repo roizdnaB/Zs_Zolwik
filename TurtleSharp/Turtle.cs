ec6a4e92a7362f93699f90a55769465eaade1ffe
Quackeee <krzysztofklak7@gmail.com>
2020-11-11 20:15:59 +0100

    implement turtle class, create presentation layer interface and fix project structure

---
+++ TurtleSharp/Turtle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TurtleSharp
+{
+    public class Turtle
+    {
+        private ITurtlePresentation Presentation;
+        private bool _isVisible = true;
+        public bool IsPlacedOnSomething => Presentation != null;
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                if (value == _isVisible) return;
+                else
+                {
+                    _isVisible = value;
+                    Presentation?.ToggleTurtleVisibility(this);
+                }
+            }
+        }
+
+        public Color PenColor = Color.Black; //Brush?
+        public double PenSize = 1;
+        public bool PenActive = true;
+
+        public void Place(ITurtlePresentation presentation)
+        {
+            Presentation?.RemoveTurtle(this);
+
+            Presentation = presentation;
+            presentation.PlaceTurtle(this);
+        }
+
+        public void TakeOff()
+        {
+            if (IsPlacedOnSomething)
+            {
+                Presentation.RemoveTurtle(this);
+                Presentation = null;
+            }
+        }
+
+        public void Forward(double distance) => Presentation?.TurtleForward(this, distance);
+        public void Backward(double distance) => Presentation?.TurtleBackward(this, distance);
+        public void Rotate(double degrees) => Presentation?.TurtleRotate(this, degrees);
+        public void Curve(double radius, double length) => Presentation?.TurtleCurve(this, radius, length);
+        public void Circle(double radius) => Presentation?.TurtleCurve(this, radius, 2 * Math.PI * radius);
+        public void Square(double sideLength)
+        {
+            if (IsPlacedOnSomething)
+            {
+                Forward(sideLength);
+                Rotate(90);
+                Forward(sideLength);
+                Rotate(90);
+                Forward(sideLength);
+                Rotate(90);
+                Forward(sideLength);
+                Rotate(90);
+            }
+        }
+        public void Triangle(double sideLength)
+        {
+            if (IsPlacedOnSomething)
+            {
+                Forward(sideLength);
+                Rotate(60);
+                Forward(sideLength);
+                Rotate(60);
+                Forward(sideLength);
+                Rotate(60);
+            }
+        }
+        public void GoHome() => Presentation?.TurtleReset(this);
+    }
+}
--- Zolwik/Zolwik/TurtleSharp/ITurtlePresentation.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TurtleSharp
-{
-    public interface ITurtlePresentation
-    {
-    }
-}
