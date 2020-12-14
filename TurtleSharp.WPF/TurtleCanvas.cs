0a4626c1498ca11badaf3cfb97a0e62f3f513549
Natalia <nszarek284@gmail.com>
2020-12-14 21:15:26 +0100

    Menu in view (doesn't do anything) + impl. interface

--- TurtleSharp.WPF/TurtleCanvas.cs
+++ TurtleSharp.WPF/TurtleCanvas.cs
@@ -21,5 +21,50 @@ namespace TurtleSharp.WPF
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TurtleCanvas), new FrameworkPropertyMetadata(typeof(TurtleCanvas)));
         }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceTurtle(Turtle turtle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTurtle(Turtle turtle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleTurtleVisibility(Turtle turtle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurtleBackward(Turtle turtle, double distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurtleCurve(Turtle turtle, double radius, double length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurtleForward(Turtle turtle, double distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurtleReset(Turtle turtle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurtleRotate(Turtle turtle, double degrees)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
