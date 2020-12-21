96ebdcb4d9c608822b49601a5c5449340b9e0e25
Natalia <nszarek284@gmail.com>
2020-12-21 21:20:01 +0100

    Resources + Menu Items: settings (not the final version) + about (probably not the final version)

--- Zolwik/ViewModels/MenuVM.cs
+++ Zolwik/ViewModels/MenuVM.cs
@@ -6,16 +6,22 @@ using System.Windows;
 using MVVM.ViewModel;
 using System.Windows.Input;
 using System.IO;
+using Zolwik.DialogBoxes;
 
 namespace Zolwik.ViewModels
 {
      class MenuVM : ViewModelBase
     {
         private string _text = String.Empty;
+        private string _path = null;
+        public string Path { get => _path; set => _path = value; }
         public string Text { get => _text; set { _text = value; OnPropertyChanged(nameof(Text)); } }
         public RelayCommand LoadTextFromFile { get; private set; }
         public RelayCommand SaveTextFromFile { get; private set; }
+        public RelayCommand SaveAsTextFromFile { get; private set; }
+        public RelayCommand CopyText { get; private set; }
 
+        public RelayCommand About { get; private set; }
 
         private void _loadTextFromFileCommand(object path)
         {
@@ -27,17 +33,50 @@ namespace Zolwik.ViewModels
             }
         }
 
-        private void _saveTextFromFileCommand(object path)
+        private void _saveAsTextFromFileCommand(object path) 
         {
             string FilePath = path as string;
 
-            File.WriteAllText(FilePath,Text);
+            File.WriteAllText(FilePath, Text);
+            Path = FilePath;
         }
+
+        private void _saveTextFromFileCommand(object path)
+        {
+            string FilePath = path as string;
+            if (Path == null)
+            {
+                Path = FilePath;
+            }
+                File.WriteAllText(Path, Text);
+            
+        }
+
+        private void _copyTextCommand()
+        {
+            Clipboard.Clear();
+            Clipboard.SetData(DataFormats.UnicodeText, Text);
+        }
+
+        private void _aboutCommand()
+        {
+            var dialogBox = new MessageDialogBox()
+            {
+                Caption = "O projekcie",
+                Icon = System.Windows.MessageBoxImage.Warning,
+                Buttons = System.Windows.MessageBoxButton.OK
+        };
+            dialogBox.showMessageBox("Projekt zaliczeniowy z przedmiotu Inżynieria Oprogramowania. \n\n Natalia Szarek, Krzysztof Kłak, Daniel Jambor");
+        }
+
+
         public MenuVM()
         {
             LoadTextFromFile = new RelayCommand(arg => _loadTextFromFileCommand(arg));
             SaveTextFromFile = new RelayCommand(arg => _saveTextFromFileCommand(arg));
-
+            SaveAsTextFromFile = new RelayCommand(arg => _saveAsTextFromFileCommand(arg));
+            CopyText = new RelayCommand(arg => _copyTextCommand());
+            About = new RelayCommand(arg => _aboutCommand());
         }
     }
 
