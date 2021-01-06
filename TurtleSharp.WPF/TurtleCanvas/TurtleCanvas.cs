5830145abc71cbee6fe0c69b9fc05ca2e3a0db94
Natalia <nszarek284@gmail.com>
2021-01-06 22:50:00 +0100

    Turtle is a turtle

--- TurtleSharp.WPF/TurtleCanvas/TurtleCanvas.cs
+++ TurtleSharp.WPF/TurtleCanvas/TurtleCanvas.cs
@@ -25,7 +25,7 @@ namespace TurtleSharp.WPF
             //Calculate the coords of the middle of the canvas
             var xCenter = 0;// (200 / 2);
             var yCenter = 0;// 200 / 2;
-
+            /*
             //Set the shape of the turtle
             Point Point1 = new Point(xCenter - 10, yCenter - 10);
             Point Point2 = new Point(xCenter + 10, yCenter - 10);
@@ -38,6 +38,53 @@ namespace TurtleSharp.WPF
             myPointCollection.Add(Point2);
             myPointCollection.Add(Point3);
             myPointCollection.Add(Point4);
+            */
+            Point PointA = new Point(xCenter - 12, yCenter);
+            Point PointB = new Point(xCenter - 8, yCenter - 4);
+            Point PointC = new Point(xCenter - 10, yCenter - 10);
+            Point PointD = new Point(xCenter - 4, yCenter - 8);
+            Point PointE = new Point(xCenter, yCenter - 12);
+            Point PointF = new Point(xCenter + 3, yCenter - 11);
+            Point PointG = new Point(xCenter + 9, yCenter - 14);
+            Point PointH = new Point(xCenter + 9, yCenter - 9);
+            Point PointI = new Point(xCenter + 12, yCenter - 8);
+            Point PointJ = new Point(xCenter + 12, yCenter - 4);
+            Point PointK = new Point(xCenter + 16, yCenter - 4);
+            Point PointL = new Point(xCenter + 20, yCenter);
+            Point PointM = new Point(xCenter + 16, yCenter + 4);
+            Point PointN = new Point(xCenter + 12, yCenter + 4);
+            Point PointO = new Point(xCenter + 12, yCenter + 8);
+            Point PointP = new Point(xCenter + 9, yCenter + 9);
+            Point PointR = new Point(xCenter + 9, yCenter + 14);
+            Point PointS = new Point(xCenter + 3, yCenter + 11);
+            Point PointT = new Point(xCenter, yCenter + 12);
+            Point PointU = new Point(xCenter - 4, yCenter + 8);
+            Point PointW = new Point(xCenter - 10, yCenter + 10);
+            Point PointZ = new Point(xCenter - 8, yCenter + 4);
+
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(PointA);
+            myPointCollection.Add(PointB);
+            myPointCollection.Add(PointC);
+            myPointCollection.Add(PointD);
+            myPointCollection.Add(PointE);
+            myPointCollection.Add(PointF);
+            myPointCollection.Add(PointG);
+            myPointCollection.Add(PointH);
+            myPointCollection.Add(PointI);
+            myPointCollection.Add(PointJ);
+            myPointCollection.Add(PointK);
+            myPointCollection.Add(PointL);
+            myPointCollection.Add(PointM);
+            myPointCollection.Add(PointN);
+            myPointCollection.Add(PointO);
+            myPointCollection.Add(PointP);
+            myPointCollection.Add(PointR);
+            myPointCollection.Add(PointS);
+            myPointCollection.Add(PointT);
+            myPointCollection.Add(PointU);
+            myPointCollection.Add(PointW);
+            myPointCollection.Add(PointZ);
 
             //Show the turtle
             _turtleRep.Points = myPointCollection;
